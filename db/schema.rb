7260e0f06f0b5f5cb024c49a58370ddf0689d64c
RITZ-pocket <86193135+RITZ-pocket@users.noreply.github.com>
2021-07-15 20:35:48 +0900

    カラム名のスペルミスを修正

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2021_07_15_021422) do
     t.string "family_name_kana", null: false
     t.string "first_name", null: false
     t.string "first_name_kana", null: false
-    t.date "borth_date", null: false
+    t.date "birth_date", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
