66de664c8f8178031c27859f04dc525fcc4c5a1f
RITZ-pocket <86193135+RITZ-pocket@users.noreply.github.com>
2021-07-17 20:04:45 +0900

    FactoryBotにuserぼダミーを定義

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {'test'}
+    email                 {'test@example'}
+    password              {'000000'}
+    password_confirmation {password}
+    family_name           ('苗字')
+    family_name_kana      {'ミョウジ'}
+    first_name            {'名前'}
+    first_name_kana       {'ナマエ'}
+    birth_date            {'2021-07-17'}
+  end
+end
