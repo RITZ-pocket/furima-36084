f8518542189b10db3e44ea31c28c9e9f9fa86300
RITZ-pocket <86193135+RITZ-pocket@users.noreply.github.com>
2021-07-19 15:41:28 +0900

    バリデーションの整理

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,10 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :family_name, presence: true
-  validates :family_name_kana, presence: true
   validates :first_name, presence: true
+
+  validates :family_name_kana, presence: true
   validates :first_name_kana, presence: true
+  
   validates :birth_date, presence: true
 end
