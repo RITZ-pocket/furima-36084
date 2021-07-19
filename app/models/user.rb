e1c6dec2a75c3f3c2c2ea178aac9b63a2ce56cbb
RITZ-pocket <86193135+RITZ-pocket@users.noreply.github.com>
2021-07-19 16:36:28 +0900

    名字・名前にそれぞれ全角バリデーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
 
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+    validates :family_name
+    validates :first_name
+
+  with_options presence: true , format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :family_name_kana
+    validates :first_name_kana
 
   validates :birth_date, presence: true
 
