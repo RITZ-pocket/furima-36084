55c6a243d63c3ec3b06f59973e3fdd1fed2f52d2
RITZ-pocket <86193135+RITZ-pocket@users.noreply.github.com>
2021-07-21 18:12:07 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,8 +5,15 @@ class Item < ApplicationRecord
   belongs_to :item_condition
   belongs_to :postage_payer
   belongs_to :shipping_area
-  
+
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :name, :price, :introduction
+    with_options numericality: { other_than:1} do
+      validates :category_id, :day_to_ship_id, :item_condition_id, :postage_payer_id, :shipping_area_id
+    end
+  end
   
 end
