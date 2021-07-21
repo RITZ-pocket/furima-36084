7cbe0d3bac63185fea35d84b263d58bb696c52da
RITZ-pocket <86193135+RITZ-pocket@users.noreply.github.com>
2021-07-21 17:14:06 +0900

    モデル毎のアソシエーションを記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,11 @@
 class Item < ApplicationRecord
-
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :day_to_ship
+  belongs_to :item_condition
+  belongs_to :postage_payer
+  belongs_to :shipping_area
+  
   belongs_to :user
   has_one_attached :image
   
