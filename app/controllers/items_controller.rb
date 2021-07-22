6700829a5c15847db1a72cffd7d64c4c32ad00a7
RITZ-pocket <86193135+RITZ-pocket@users.noreply.github.com>
2021-07-22 17:49:14 +0900

    mergeを記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,6 +19,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :price, :introduction, :category_id, :day_to_ship_id, :item_condition_id, :postage_payer_id, :shipping_area_id, :image)
+    params.require(:item).permit(:name, :price, :introduction, :category_id, :day_to_ship_id, :item_condition_id, :postage_payer_id, :shipping_area_id, :image).merge(user_id: current_user.id)
   end
 end
