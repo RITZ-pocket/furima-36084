eb37b9c4cce1ba8b5b90ded1f60217c75627471d
RITZ-pocket <86193135+RITZ-pocket@users.noreply.github.com>
2021-07-22 17:38:06 +0900

    スペルミス修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,6 +8,7 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
+    @item = Item.create(item_params)
     if @item.save
       redirect_to root_path
     else
@@ -18,6 +19,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :price, :introduction, :category_id, :day_to_ship_id, :item_condition_id, :postage_payer_id, :shipping_area_id)
+    params.require(:item).permit(:name, :price, :introduction, :category_id, :day_to_ship_id, :item_condition_id, :postage_payer_id, :shipping_area_id, :image)
   end
 end
