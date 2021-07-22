7cce1ac73552cae8f170ea5b3f1f87ea09d7ff5d
RITZ-pocket <86193135+RITZ-pocket@users.noreply.github.com>
2021-07-22 14:58:04 +0900

    出品画面で入力した情報を保存できるよう記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,23 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:article).permit(:name, :price, :introduction, :category_id, :day_to_ship_id, :item_condition_id, :postage_payer_id, :shipping_area_id)
+  end
 end
