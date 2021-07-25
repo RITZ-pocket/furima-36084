ceb0b7e44a4ea20a11a2d8f3877ce2371368111d
RITZ-pocket <86193135+RITZ-pocket@users.noreply.github.com>
2021-07-25 19:49:11 +0900

    余分に記述していたバリデーションの削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,7 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name, :introduction, :image
     with_options format: { with: /\A[0-9]+\z/ } do
-      validates :price, presence: true,
+      validates :price,
                         numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
     end
     with_options numericality: { other_than: 1, message: "can't be blank" } do
