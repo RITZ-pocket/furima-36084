3c8a5118475cf81f8adfd78015792dfe6a1645b7
RITZ-pocket <86193135+RITZ-pocket@users.noreply.github.com>
2021-07-25 19:45:48 +0900

    authenticate_user!メソッドで非ログイン時の商品登録ページのアクセスを制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :move_to_login, except: [:index]
+  before_action :authenticate_user!, except: [:index]
 
   def index
   end
@@ -23,9 +23,5 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :price, :introduction, :category_id, :day_to_ship_id, :item_condition_id,
                                  :postage_payer_id, :shipping_area_id, :image).merge(user_id: current_user.id)
-  end
-
-  def move_to_login
-    redirect_to new_user_session_path unless user_signed_in?
   end
 end
