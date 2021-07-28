86e553d03fbe42d80cb626fd45eebab01c73c384
RITZ-pocket <86193135+RITZ-pocket@users.noreply.github.com>
2021-07-28 18:26:41 +0900

    ルーティングのネストを行い、購入ページのビューを出品データに変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,14 @@
 class OrdersController < ApplicationController
 
   def index
+    @items = Item.find(params[:item_id])
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price, :introduction, :category_id, :day_to_ship_id, :item_condition_id,
+                                 :postage_payer_id, :shipping_area_id, :image).merge(user_id: current_user.id)
   end
   
 end
