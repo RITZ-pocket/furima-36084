4e505da5c955025e414d28de12eb2753e1e5c4f5
RITZ-pocket <86193135+RITZ-pocket@users.noreply.github.com>
2021-07-28 20:32:28 +0900

    shipモデルのバリデーションを設定

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,11 @@
 class Ship < ApplicationRecord
+  with_options presence: true do
+    validates :city, :address
+    with_options format: { with: /\A\d{3}[-]\d{4}\z/ } do
+      validates :post_code
+    end
+    with_options format: { with: /\A\d{10,11}\z/ } do
+      validates :phone_number
+    end
+  end
 end
