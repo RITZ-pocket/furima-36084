4796b23dcec5b80ed7d3bf941a5f9d60e49063f4
RITZ-pocket <86193135+RITZ-pocket@users.noreply.github.com>
2021-07-30 12:01:55 +0900

    shipsテーブルにshipping_area_idカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_28_094258) do
+ActiveRecord::Schema.define(version: 2021_07_28_093540) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2021_07_28_094258) do
 
   create_table "ships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "post_code", null: false
+    t.integer "shipping_area_id", null: false
     t.string "city", null: false
     t.string "address", null: false
     t.string "building_name"
