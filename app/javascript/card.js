4500f7ba91938f7754ad27d9321a134ea0ecc45e
RITZ-pocket <86193135+RITZ-pocket@users.noreply.github.com>
2021-08-02 15:39:59 +0900

    tokenの設定

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,24 +1,33 @@
 const pay = () => {
-  payjp.setPublicKey("");
-  const form = document.getElementById("charge-form");
-  form.addEventListener("submit", (e) => {
+  Payjp.setPublicKey("");
+  const submit = document.getElementById("button");
+  submit.addEventListener("click", (e) => {
     e.preventDefault();
 
     const formResult = document.getElementById("charge-form");
     const formData = new FormData(formResult);
 
     const card = {
-      number: formData.get("order[number]"),
-      cvc: formData.get("order[cvc]"),
-      exp_month: formData.get("order[exp_month]"),
-      exp_year: `20${formData.get("order[exp_year]")}`,
+      number: formData.get("ship_purchase[number]"),
+      cvc: formData.get("ship_purchase[cvc]"),
+      exp_month: formData.get("ship_purchase[exp_month]"),
+      exp_year: `20${formData.get("ship_purchase[exp_year]")}`,
     };
 
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
         const token = response.id;
-        console.log(token)
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden"> `;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
+
+      document.getElementById("ship_purchase_number").removeAttribute("name");
+      document.getElementById("ship_purchase_cvc").removeAttribute("name");
+      document.getElementById("ship_purchase_exp_month").removeAttribute("name");
+      document.getElementById("ship_purchase_exp_year").removeAttribute("name");
+
+      document.getElementById("charge-form").submit();
     });
   });
 };
