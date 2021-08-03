cb7fcc425870f00eefd9b7232318b3bdc13cf9ad
RITZ-pocket <86193135+RITZ-pocket@users.noreply.github.com>
2021-08-03 19:04:22 +0900

    不要なバリデーションの削除

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -2,16 +2,4 @@ class Ship < ApplicationRecord
 
   belongs_to :purchase
   
-  with_options presence: true do
-    validates :city, :address
-    with_options format: { with: /\A\d{3}[-]\d{4}\z/ } do
-      validates :post_code
-    end
-    with_options format: { with: /\A\d{10,11}\z/ } do
-      validates :phone_number
-    end
-    with_options numericality: {other_than: 1, message: "can't be blank"} do
-      validates :shipping_area_id
-    end
-  end
 end
